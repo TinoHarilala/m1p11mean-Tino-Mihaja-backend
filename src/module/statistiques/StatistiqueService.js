371ac1234f0e49c96ed9582fafb409c46e5e6acc
Mihaja1 <mihajaraman@gmail.com>
2024-02-24 23:11:14 +0300

    Update StatistiqueService.js

--- src/module/statistiques/StatistiqueService.js
+++ src/module/statistiques/StatistiqueService.js
@@ -61,36 +61,71 @@ class StatistiqueService {
 
         pipeline.push({ $sort: { 'date': -1 } });
 
-        const resultats = await RendezVousEmploye.aggregate([
-            {
-                $group: {
-                    _id: {
-                        date: { $dateToString: { format: "%Y-%m", date: "$date" } },
-                        employe: "$idEmploye"
-                    },
-                    totalHours: {
-                        $sum: {
-                            $divide: [{ $subtract: ["$endTime", "$startTime"] }, 3600000] 
-                        }
-                    }
-                }
+        // const resultats = await RendezVousEmploye.aggregate([
+        //     {
+        //         $group: {
+        //             _id: {
+        //                 date: { $dateToString: { format: "%Y-%m", date: "$date" } },
+        //                 employe: "$idEmploye"
+        //             },
+        //             totalHours: {
+        //                 $sum: {
+        //                     $divide: [{ $subtract: ["$endTime", "$startTime"] }, 3600000] 
+        //                 }
+        //             }
+        //         }
+        //     },
+        //     {
+        //         $project: {
+        //             date: "$_id.date",
+        //             employe: "$_id.employe",
+        //             totalHours: 1,
+        //             _id: 0
+        //         }
+        //     },
+        //     {
+        //         $addFields: {
+        //             year: { $year: { $toDate: "$date" } }, // Ajouter l'année de la date
+        //             month: { $month: { $toDate: "$date" } } // Ajouter le mois de la date
+        //         }
+        //     }
+        // ]);
+const resultats = await RendezVousEmploye.aggregate([
+    {
+        $group: {
+            _id: {
+                date: { $dateToString: { format: "%Y-%m", date: "$date" } },
+                employe: "$idEmploye"
             },
-            {
-                $project: {
-                    date: "$_id.date",
-                    employe: "$_id.employe",
-                    totalHours: 1,
-                    _id: 0
-                }
-            },
-            {
-                $addFields: {
-                    year: { $year: { $toDate: "$date" } }, // Ajouter l'année de la date
-                    month: { $month: { $toDate: "$date" } } // Ajouter le mois de la date
-                }
+            totalMilliseconds: {
+                $sum: { $subtract: ["$endTime", "$startTime"] }
             }
-        ]);
-
+        }
+    },
+    {
+        $project: {
+            date: "$_id.date",
+            employe: "$_id.employe",
+            totalHours: { $divide: ["$totalMilliseconds", 3600000] }, // Convertir les millisecondes en heures
+            _id: 0
+        }
+    },
+    {
+        $group: {
+            _id: "$employe",
+            totalHours: { $sum: "$totalHours" }, // Somme des heures de travail pour chaque employé
+            count: { $sum: 1 } // Compter le nombre d'enregistrements pour chaque employé (pour obtenir le nombre de mois)
+        }
+    },
+    {
+        $project: {
+            employe: "$_id",
+            totalHours: 1,
+            averageHours: { $divide: ["$totalHours", { $multiply: ["$count", 176] }] }, // Division par le nombre total d'heures dans tous les mois
+            _id: 0
+        }
+    }
+]);
 
 
 
